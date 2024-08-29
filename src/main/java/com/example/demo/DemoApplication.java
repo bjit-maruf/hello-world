63b2424e694a0e8cf0614ebfe53e35deb3019514
maruf.diu <maruf15-5445@diu.edu.bd>
2024-08-29 17:15:02 +0600

    Demo commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		// comment added1ddd444555
 		System.out.println("Hello world");
-		// another commit
+		// another commit2
 	}
 
 }
