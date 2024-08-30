42c4abcee1f61c3796c06a2e804b9523d5ad9993
Horidas Roy <horidas.roy@bjitgroup.com>
2024-08-30 10:21:06 +0600

    Logger added.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,19 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	public  static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		// comment added1ddd444555
 		System.out.println("Hello world");
+		logger.info("Logger Added.");
 		// another commit
 	}
 
