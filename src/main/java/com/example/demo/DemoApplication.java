1e51feef709306ecdf080ff06fbc4745ee984a3b
ekramul.hoque <ekramul.hoque@bjitgroup.com>
2024-08-30 11:33:10 +0600

    Remove

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		// comment added1ddd444555
-		System.out.println("Hello world");
+		System.out.println("Hello world abc");
 		// another commit
-		// Ekram
+		// some new 
 	}
 
 }
